970ed290165e6b50ce4c2ae91d0c2ce4d8719698
krishna <kk612470@gmail.com>
2025-05-01 15:02:14 +0530

    test 3

--- server.js
+++ server.js
@@ -211,7 +211,7 @@ app.get('/', (req, res) => {
 
 
 //-----------------------------------------------------------------
-const PORT = process.env.PORT || 5000;
-app.listen(3000, () => {
-  console.log(`🚀 Server running on port ${3000}`);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
